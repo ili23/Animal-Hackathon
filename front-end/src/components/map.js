fff315a651af1fc6ac99f4b90b6dd7923b0c905b
Iram Liu <il233@cornell.edu>
2023-02-09 21:43:49 -0500

    Update map.js

--- front-end/src/components/map.js
+++ front-end/src/components/map.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/map-marker'
 
 const location = {
   address: '225 Tulip Tree Dr, Ithaca, NY 14850',
   lat: 42.447013138256445,
   lng: -76.45344569651013,
 }
-
+const LocationPin = ({ text }) => (
+  <div className="pin">
+    <Icon icon={locationIcon} className="pin-icon" />
+    <p className="pin-text">{text}</p>
+  </div>
+)
 const Map = ({ location, zoomLevel }) => (
   <div className="map">
-    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+    <h2 className="map-h2">Example Farm Map View</h2>
 
     <div className="google-map">
       <GoogleMapReact
@@ -29,7 +36,7 @@ const Map = ({ location, zoomLevel }) => (
 function MapBox() {
   return (
     <div>
-
+       <MapSection location={location} zoomLevel={17} /> {/* include it here */}
     </div>
   )
 }
